acf258edc09be4c68f1a49c5f627424731c93b3c
WillB97 <will@rudi.org.uk>
2021-04-03 20:24:42 +0100

    Redo default config using python

--- init.sh
+++ init.sh
@@ -80,7 +80,13 @@ cd srcomp-cli
 cd ..
 cd srcomp-screens
     bower install
-    sed 's_localhost/_localhost:5112/_' config.example.json >config.json
+    python -c '
+import sys, json
+print(json.dumps({
+    **json.load(sys.stdin),
+    "apiurl": "http://localhost:5112/comp-api",
+    "streamurl": "http://localhost:5001/"
+}, indent=2))' <config.example.json >config.json
 cd ..
 cd srcomp-stream
     sed 's_SRCOMP: .*_SRCOMP: "http://localhost:5112/comp-api"_' <config.local.coffee.example >config.local.coffee
